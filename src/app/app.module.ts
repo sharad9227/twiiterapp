81dc0b914c23b41576b74f8f150c023bb0296c65
sharadroy-choudhury <sharad.src42@gmail.com>
2021-10-07 16:14:24 +0100

    registration changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { RegisterComponent } from './register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/models/User.ts
@@ -0,0 +1,11 @@
+export class User {
+  id: number;
+  username: string;
+  password: string;
+  confirmPassword:string;
+  contact:number;
+  email:string;
+  firstName: string;
+  lastName: string;
+  token: string;
+}
